12c34399a42792dd903460c29dd445f82423d62f
m-dantas <mauricio.dantascp@gmail.com>
2024-02-13 22:55:11 -0300

    ajuste tab

--- teste.sql
+++ teste.sql
@@ -4,13 +4,13 @@ GO
 /****** Object:  Schema [APC]    Script Date: 10/02/2024 18:33:31 ******/
 IF NOT EXISTS (SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'APC')
 BEGIN
-    EXEC('CREATE SCHEMA APC')
+	EXEC('CREATE SCHEMA APC')
 END
 GO
 
 CREATE TABLE APC.Segmentos (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL,
 	status BIT NOT NULL,
 	createdAt DATETIMEOFFSET NOT NULL,
 	updatedAt DATETIMEOFFSET NOT NULL
@@ -18,8 +18,8 @@ CREATE TABLE APC.Segmentos (
 
 -- Tabela Produto
 CREATE TABLE APC.Produtos (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
+  id INT IDENTITY(1,1) PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
 	status BIT NOT NULL,
 	createdAt DATETIMEOFFSET NOT NULL,
 	updatedAt DATETIMEOFFSET NOT NULL,
@@ -29,23 +29,23 @@ CREATE TABLE APC.Produtos (
 
 -- Tabela Arquivo
 CREATE TABLE APC.Arquivos (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    valor VARCHAR(255) NOT NULL,
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL,
+	valor VARCHAR(255) NOT NULL,
 	url BIT NOT NULL,
 	usuario VARCHAR(255) NOT NULL,
 	createdAt DATETIMEOFFSET NOT NULL,
 	updatedAt DATETIMEOFFSET NOT NULL,
-    idProduto INT NOT NULL,
-    FOREIGN KEY (idProduto) REFERENCES APC.Produtos(id)
+	idProduto INT NOT NULL,
+	FOREIGN KEY (idProduto) REFERENCES APC.Produtos(id)
 );
 
 CREATE TABLE APC.Autorizacoes (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    usuario VARCHAR(255) NOT NULL,
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	usuario VARCHAR(255) NOT NULL,
 	admin BIT NOT NULL,
 	createdAt DATETIMEOFFSET NOT NULL,
 	updatedAt DATETIMEOFFSET NOT NULL,
-    idSegmento INT NOT NULL,
-    FOREIGN KEY (idSegmento) REFERENCES APC.Segmentos(id)
+	idSegmento INT NOT NULL,
+	FOREIGN KEY (idSegmento) REFERENCES APC.Segmentos(id)
 );
